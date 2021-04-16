16ea92dad8559082eab7dea4612125b0b3741393
Bartłomiej Popiołek <popiolekbartlomiej369@gmail.com>
2021-04-16 17:54:08 +0200

    Simplified logic into CompanyInfo component

--- src/components/moldecules/CopmanyInfo/CompanyInfo.js
+++ src/components/moldecules/CopmanyInfo/CompanyInfo.js
@@ -14,41 +14,37 @@ const CompanyInfo = () => {
 
     return (
         <Wrapper>
-            {lengthCompanyInfo <= 1 && <div>No company info...</div>}
             {isLoading ? (
                 <div>Loading info about company...</div>
+            ) : lengthCompanyInfo > 1 ? (
+                <>
+                    <Title>{companyInfo.Name}</Title>
+                    <StyledInfoCard>
+                        <div>
+                            <Subtitle>Symbol:</Subtitle> {companyInfo.Symbol}
+                            <Subtitle>Sector: </Subtitle> {companyInfo.Sector}
+                            <Subtitle>Description:</Subtitle>
+                            <StyledDescription>
+                                {companyInfo.Description}
+                            </StyledDescription>
+                        </div>
+                        <div>
+                            <Subtitle>Currency: </Subtitle>{" "}
+                            {companyInfo.Currency}
+                            <Subtitle>Country: </Subtitle> {companyInfo.Country}
+                            <Subtitle>Fulltime employees: </Subtitle>
+                            {companyInfo.FullTimeEmployees}
+                            <Subtitle>Market capitalization: </Subtitle>$
+                            {companyInfo.MarketCapitalization}
+                            <Subtitle>Analyst target price: </Subtitle>$
+                            {companyInfo.AnalystTargetPrice}
+                            <Subtitle>Profit margin: </Subtitle>$
+                            {companyInfo.ProfitMargin}
+                        </div>
+                    </StyledInfoCard>
+                </>
             ) : (
-                lengthCompanyInfo > 1 && (
-                    <>
-                        <Title>{companyInfo.Name}</Title>
-                        <StyledInfoCard>
-                            <div>
-                                <Subtitle>Symbol:</Subtitle>{" "}
-                                {companyInfo.Symbol}
-                                <Subtitle>Sector: </Subtitle>{" "}
-                                {companyInfo.Sector}
-                                <Subtitle>Description:</Subtitle>
-                                <StyledDescription>
-                                    {companyInfo.Description}
-                                </StyledDescription>
-                            </div>
-                            <div>
-                                <Subtitle>Currency: </Subtitle>{" "}
-                                {companyInfo.Currency}
-                                <Subtitle>Country: </Subtitle>{" "}
-                                {companyInfo.Country}
-                                <Subtitle>Fulltime employees: </Subtitle>
-                                {companyInfo.FullTimeEmployees}
-                                <Subtitle>Market capitalization: </Subtitle>$
-                                {companyInfo.MarketCapitalization}
-                                <Subtitle>Analyst target price: </Subtitle>$
-                                {companyInfo.AnalystTargetPrice}
-                                <Subtitle>Profit margin: </Subtitle>$
-                                {companyInfo.ProfitMargin}
-                            </div>
-                        </StyledInfoCard>
-                    </>
-                )
+                <div>No company info...</div>
             )}
         </Wrapper>
     );
